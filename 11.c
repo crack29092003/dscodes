df7307d81b970d80aa03447a4982d24de9b4611a
madan@29 <164165412+crack29092003@users.noreply.github.com>
2024-03-21 22:17:40 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -3,201 +3,158 @@
 #include <ctype.h>
 #include <string.h>
 
-// Structure to represent a stack
+#define MAX_EXPR_SIZE 100
+
 struct Stack {
     int top;
     unsigned capacity;
     char* array;
 };
 
-// Function to create a stack of given capacity
 struct Stack* createStack(unsigned capacity) {
     struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
-    if (!stack)
-        return NULL;
-    stack->top = -1;
     stack->capacity = capacity;
+    stack->top = -1;
     stack->array = (char*)malloc(stack->capacity * sizeof(char));
-    if (!stack->array) {
-        free(stack);
-        return NULL;
-    }
     return stack;
 }
 
-// Function to check if the stack is full
-int isFull(struct Stack* stack) {
-    return stack->top == stack->capacity - 1;
-}
-
-// Function to check if the stack is empty
 int isEmpty(struct Stack* stack) {
     return stack->top == -1;
 }
 
-// Function to push an element to the stack
-void push(struct Stack* stack, char item) {
-    if (isFull(stack))
-        return;
-    stack->array[++stack->top] = item;
-}
-
-// Function to pop an element from the stack
-char pop(struct Stack* stack) {
-    if (isEmpty(stack))
-        return -1;
-    return stack->array[stack->top--];
-}
-
-// Function to get the top element of the stack without removing it
 char peek(struct Stack* stack) {
-    if (isEmpty(stack))
-        return -1;
     return stack->array[stack->top];
 }
 
-// Function to get the precedence of an operator
-int precedence(char op) {
-    if (op == '(')
-        return 0;
-    if (op == '+' || op == '-')
-        return 1;
-    if (op == '*' || op == '/')
-        return 2;
+char pop(struct Stack* stack) {
+    if (!isEmpty(stack))
+        return stack->array[stack->top--];
+    return '$';
+}
+
+void push(struct Stack* stack, char op) {
+    stack->array[++stack->top] = op;
+}
+
+int isOperand(char ch) {
+    return isalnum(ch);
+}
+
+int precedence(char ch) {
+    switch (ch) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        case '^':
+            return 3;
+    }
     return -1;
 }
 
-// Function to reverse a string
 void reverseString(char* str) {
-    int left = 0, right = strlen(str) - 1;
-    while (left < right) {
-        char temp = str[left];
-        str[left] = str[right];
-        str[right] = temp;
-        left++;
-        right--;
+    int length = strlen(str);
+    int start = 0;
+    int end = length - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
     }
 }
 
-// Function to convert infix expression to prefix expression
-char* infixToPrefix(char* exp) {
-    struct Stack* stack = createStack(strlen(exp));
-    char* prefix = (char*)malloc((strlen(exp) + 1) * sizeof(char));
-    int i, j = 0;
-
-    if (!stack || !prefix) {
-        free(stack);
-        free(prefix);
-        return NULL;
-    }
-
-    reverseString(exp);
-
-    for (i = 0; exp[i]; ++i) {
-        if (exp[i] == '(') {
-            exp[i] = ')';
-            i++;
-        } else if (exp[i] == ')') {
-            exp[i] = '(';
-            i++;
-        }
-    }
-
-    for (i = 0; exp[i]; ++i) {
-        if (isdigit(exp[i])) {
-            prefix[j++] = exp[i];
-        } else if (exp[i] == '(') {
-            push(stack, exp[i]);
-        } else if (exp[i] == ')') {
-            while (!isEmpty(stack) && peek(stack) != '(') {
-                prefix[j++] = pop(stack);
-            }
-            pop(stack); // Remove '('
-        } else { // Operator
-            while (!isEmpty(stack) && precedence(exp[i]) < precedence(peek(stack))) {
-                prefix[j++] = pop(stack);
-            }
-            push(stack, exp[i]);
-        }
-    }
-
-    while (!isEmpty(stack)) {
-        prefix[j++] = pop(stack);
-    }
-    prefix[j] = '\0';
-
-    reverseString(prefix);
-    free(stack->array);
-    free(stack);
-    return prefix;
-}
-
-// Function to evaluate the prefix expression
-int evaluatePrefix(char* exp) {
-    struct Stack* stack = createStack(strlen(exp));
-    int i, result;
-
-    if (!stack)
-        return -1;
-
-    for (i = strlen(exp) - 1; i >= 0; --i) {
-        if (isdigit(exp[i])) {
-            push(stack, exp[i] - '0');
+void infixToPrefix(char* infix, char* prefix) {
+    struct Stack* stack = createStack(strlen(infix));
+    int i, k;
+    for (i = strlen(infix) - 1, k = 0; i >= 0; --i) {
+        if (isOperand(infix[i]))
+            prefix[k++] = infix[i];
+        else if (infix[i] == ')')
+            push(stack, infix[i]);
+        else if (infix[i] == '(') {
+            while (!isEmpty(stack) && peek(stack) != ')')
+                prefix[k++] = pop(stack);
+            pop(stack);
         } else {
-            int val1 = pop(stack);
-            int val2 = pop(stack);
-            switch (exp[i]) {
-            case '+': push(stack, val1 + val2); break;
-            case '-': push(stack, val1 - val2); break;
-            case '*': push(stack, val1 * val2); break;
-            case '/': push(stack, val1 / val2); break;
+            while (!isEmpty(stack) && precedence(infix[i]) < precedence(peek(stack)))
+                prefix[k++] = pop(stack);
+            push(stack, infix[i]);
+        }
+    }
+    while (!isEmpty(stack))
+        prefix[k++] = pop(stack);
+    prefix[k] = '\0';
+    reverseString(prefix);
+}
+
+int evaluatePrefix(char* prefix) {
+    struct Stack* stack = createStack(strlen(prefix));
+    int i, op1, op2;
+    for (i = 0; prefix[i]; ++i) {
+        if (isdigit(prefix[i]))
+            push(stack, prefix[i] - '0');
+        else {
+            op1 = pop(stack);
+            op2 = pop(stack);
+            switch (prefix[i]) {
+                case '+':
+                    push(stack, op1 + op2);
+                    break;
+                case '-':
+                    push(stack, op1 - op2);
+                    break;
+                case '*':
+                    push(stack, op1 * op2);
+                    break;
+                case '/':
+                    push(stack, op1 / op2);
+                    break;
             }
         }
     }
-    result = pop(stack);
-    free(stack->array);
-    free(stack);
-    return result;
+    return pop(stack);
 }
 
 int main() {
-    char infix[100], *prefix;
+    char infix[MAX_EXPR_SIZE];
+    char prefix[MAX_EXPR_SIZE];
     int result;
     int choice;
 
     while (1) {
         printf("\nMenu:\n");
-        printf("1. Convert infix expression to prefix\n");
-        printf("2. Evaluate prefix expression\n");
+        printf("1. Convert Infix to Prefix\n");
+        printf("2. Evaluate Prefix Expression\n");
         printf("3. Exit\n");
-        printf("Enter your choice: ");
+        printf("Enter your choice (1/2/3): ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
                 printf("Enter the infix expression: ");
                 scanf("%s", infix);
-                prefix = infixToPrefix(infix);
-                if (prefix) {
-                    printf("Prefix expression: %s\n", prefix);
-                    free(prefix);
-                } else {
-                    printf("Failed to convert infix to prefix expression\n");
-                }
+                infixToPrefix(infix, prefix);
+                printf("Prefix expression: %s\n", prefix);
                 break;
             case 2:
                 printf("Enter the prefix expression: ");
-                scanf("%s", infix);
-                result = evaluatePrefix(infix);
+                scanf("%s", prefix);
+                result = evaluatePrefix(prefix);
                 printf("Result of evaluation: %d\n", result);
                 break;
             case 3:
                 printf("Exiting...\n");
                 exit(0);
             default:
-                printf("Invalid choice\n");
+                printf("Invalid choice. Please enter a valid option.\n");
         }
     }
 
     return 0;
 }
+
